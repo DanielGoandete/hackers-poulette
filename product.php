094877857f925802ee9ade44c5531d8e76ae2501
DanielGoandete <63291047+DanielGoandete@users.noreply.github.com>
2021-03-23 15:18:02 +0100

    update

--- product.php
+++ product.php
@@ -7,7 +7,7 @@
     <title>hackers-poulette</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="assets/style.css"> 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script> -->
 </head>
   
 
@@ -17,17 +17,17 @@
     </header> 
     <div class="container-md">
         <div class="row">
-          <div class="col-sm mb-4" style="width: 300px;"> <img style="width: 270px;" src="assets/product1.jpg" alt="product">
+          <div class="col-sm mb-4" style="width: 300px;"> <img style="width: 270px;height:250px" src="assets/product1.jpg" alt="product">
              <br></br>
               <a href="error.php"><button  style="margin-left:23%;" class="btn btn-primary"  name="buy">more info...</button></a>
           </div>
           
-          <div class="col-sm mb-4" style="width: 300px;"><img style="width: 270px;" src="assets/product2.jpg" alt="product">
+          <div class="col-sm mb-4" style="width: 300px;"><img style="width: 270px;height:250px" src="assets/product2.jpg" alt="product">
           <br></br>
           <a href="error.php"><button  style="margin-left:23%;" class="btn btn-primary"  name="buy">more info...</button></a>
           </div>
     
-          <div class="col-sm mb-4" style="width: 300px;"><img style="width: 270px;" src="assets/product3.jpg" alt="product">
+          <div class="col-sm mb-4" style="width: 300px;"><img style="width: 270px;height:250px" src="assets/product3.jpg" alt="product">
           <br></br>
           <a href="error.php"><button  style="margin-left:23%;" class="btn btn-primary"  name="buy">more info...</button></a>
           </div>
